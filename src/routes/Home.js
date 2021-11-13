9099b683086859e97e16b5c1f5af3278c35b0308
HyungJin <hyungjin0710@gmail.com>
2021-11-13 18:13:43 +0900

    Apply css to Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -17,11 +17,13 @@ function Home() {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
